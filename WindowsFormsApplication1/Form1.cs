28473844d99a5f817b4a43cd7ca61263a1bbbc7f
yuluakondrateva <yuluakondrateva@gmail.com>
2017-09-15 09:20:33 +0900

    6767

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("E12EE");
+            MessageBox.Show("E12hhEE");
             Close();
         }
     }
