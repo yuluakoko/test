e45c5b32021e22acd2b8a3c72e56aed046cc91da
yuluakondrateva <yuluakondrateva@gmail.com>
2017-09-15 09:08:59 +0900

    for "EEE"

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -19,6 +19,7 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
+            MessageBox.Show("E");
             Close();
         }
     }
